239f000aef3cc97968bf2bc54bca550d29bf0673
Deepthi Karkada <karkada.deepthi@gmail.com>
2024-01-24 11:00:36 -0800

    Get job modules via openai

--- job_server/app.py
+++ job_server/app.py
@@ -1,3 +1,4 @@
+import os
 import gradio as gr
 import pandas as pd
 import sqlalchemy as sql
@@ -29,49 +30,47 @@ PORT = mysql_config['port']
 PW = mysql_config['pw']
 DB = mysql_config['db']
 
-# connection_string = f"mysql://{USER}:{PW}@{HOST}:{PORT}/{DB}"
-
-# def get_jobs():
-#     df = pd.read_sql(
-#         """
-#     SELECT * FROM jobs
-#     """,
-#     con=connection_string
-#     )
-
-
-# import mysql.connector
-
-
-# mydb = mysql.connector.connect(
-#     host = HOST,
-#     port = PORT,
-#     user = USER,
-#     database = DB
-# )
-
-# query = "SELECT * FROM jobs"
-# jobs_df = pd.read_sql_query(query, mydb)
-# jobs_df = jobs_df.to_json()
-
 ## Using SQLAlchemy to read from the database 
 ## Ref: https://pythondata.com/quick-tip-sqlalchemy-for-mysql-and-pandas/
 connect_string = f'mysql://{USER}:{PW}@{HOST}:{PORT}/{DB}'
 sql_engine = sql.create_engine(connect_string)
 query = "SELECT * FROM jobs"
 jobs_df = pd.read_sql_query(query, sql_engine)
-#print(f'Jobs: {jobs_df.head()}')
-#jobs_df = jobs_df.to_json()
 
-# with gr.Blocks() as demo:
-#     gr.DataFrame(jobs_df)
+from openai import OpenAI 
 
-def greet(name):
-    return "Hello " + name + "!"
+os.environ["OPENAI_API_KEY"] = "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"
+
+client = OpenAI()
+
+def get_response(text):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {'role': 'user', 'content': f'Based on the context provided in the text delimited by triple backticks, produce the technologies used in the context as a python list. The context provided is: ```{text}```' }
+        ]
+    )
+    return response.choices[0].message.content
 
 with gr.Blocks() as demo:
+    with gr.Row():
+            gr.Markdown("Company")
+            gr.Markdown("Job title")
+            gr.Markdown("Job description")
+            gr.Markdown("Tasks")
     for index, row in jobs_df.iterrows():
-        #with gr.Row():
-        gr.Row(fn=greet, inputs=gr.Checkbox(label=row['description']), outputs=gr.Radio(["park", "zoo", "road"], label="tasks"))
+        #gr.Row(fn=greet, inputs=gr.Checkbox(label=row['description']), outputs=gr.Radio(["park", "zoo", "road"], label="tasks"))
+        with gr.Row():
+            gr.Markdown(f"{row['company']}")
+            gr.Markdown(f"{row['title']}")
+            gr.Markdown(f"{row['description']}")
+            responses = get_response(row['description'])
+            responses = responses.split(']')[0].split('[')[-1].split(',')
+            #print(f'Response: {responses}')
+            gr.Radio(choices=[response.lstrip("'").rstrip("'") for response in responses], label="tasks")
+        
+        ## For testing purposes
+        if index == 2:
+             break
 
 demo.launch(server_name="0.0.0.0", server_port=5004)
